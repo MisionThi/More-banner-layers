a814d4ea3a763d973402ccf8eb1167090c94ec3d
MisionThi <66966201+MisionThi@users.noreply.github.com>
2022-06-11 21:51:31 +0200

    1.19 update

--- src/main/java/net/mision_thi/morebannerlayers/mixin/LoginMixin.java
+++ src/main/java/net/mision_thi/morebannerlayers/mixin/LoginMixin.java
@@ -2,10 +2,10 @@ package net.mision_thi.morebannerlayers.mixin;
 
 import io.netty.buffer.Unpooled;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.PlayerManager;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.mision_thi.morebannerlayers.config.ModConfigs;
 import net.mision_thi.morebannerlayers.networking.ModPackets;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,10 +13,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+
 @Mixin(value = PlayerManager.class)
 public class LoginMixin {
 
-    @Inject(at = @At("TAIL"), method = "Lnet/minecraft/server/PlayerManager;onPlayerConnect(Lnet/minecraft/network/ClientConnection;Lnet/minecraft/server/network/ServerPlayerEntity;)V")
+    @Inject(at = @At("TAIL"), method = "onPlayerConnect(Lnet/minecraft/network/ClientConnection;Lnet/minecraft/server/network/ServerPlayerEntity;)V")
     private void syncBannerLayerLimit(ClientConnection connection, ServerPlayerEntity player, CallbackInfo info) {
 
         PacketByteBuf passedData = new PacketByteBuf(Unpooled.buffer());
